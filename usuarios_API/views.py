ddf076348046132c127819c6349c9785e75aacdc
Martin Galvan <md.galvan@uniandes.edu.co>
2020-10-25 16:07:37 -0500

    tokens autenticación

--- usuarios_API/views.py
+++ usuarios_API/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
 from . import serializers, models
 from .logic.logic_usuario import *
 
@@ -167,8 +170,11 @@ class usuarioApoyaFemUsernameDetail(APIView):
             return Response(serializer.data,status=status.HTTP_404_NOT_FOUND)
 
 
+
+
+
 ####################################################
-###############   USUARIO ABOGADO   ###############
+###############   USUARIO ABOGADO   ################
 ####################################################
 
 class usuarioAbogadoViewList(APIView):
