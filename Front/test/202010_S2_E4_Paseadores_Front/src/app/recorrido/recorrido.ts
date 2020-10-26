ca1988a94f125b99837c8e6a80ecb65037afbbe9
Nico Urrego <n.urrego@uniandes.edu.co>
2020-10-25 19:54:53 -0500

    reqs

--- Front/test/202010_S2_E4_Paseadores_Front/src/app/recorrido/recorrido.ts
+++ Front/test/202010_S2_E4_Paseadores_Front/src/app/recorrido/recorrido.ts
@@ -1,7 +1,11 @@
 export class Recorrido {
-    constructor(private idA : number, private calificacionGlobalA : number, private numeroCalificacionesA : number) {}
+    constructor(private idA : number, private correoA : string, private nombreA : string, private ciudadUsuarioA : string ) {}
+    
+    get id() : number { return this.idA; }
+    get correo() : string {return this.correoA;}
+    get nombre() : string {return this.nombreA;}
+    get ciudadUsuario() : string {return this.ciudadUsuarioA;}
 
-get id() : number { return this.idA; }
-get calificacionGlobal() : number { return this.calificacionGlobalA; }
-get numeroCalificaciones() : number { return this.numeroCalificacionesA; }
+get calificacionGlobal() : number { return null; }
+get numeroCalificaciones() : number { return null; }
 }
